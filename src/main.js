fbc286147d641391ce797974de7c21a705a7f84b
stanislav_nedelchev <stasnedel@gmail.com>
2024-09-09 16:19:46 +0300

    10

--- src/main.js
+++ src/main.js
@@ -7,6 +7,6 @@ import './js/projects';
 import './js/faq';
 import './js/covers';
 import './js/reviews';
-// import './js/work-together';
+import './js/work-together';
 import './js/modal-menu';
 import './js/modal-work-together';
